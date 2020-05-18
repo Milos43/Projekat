c99a0692470df940690ed82c20a307497b455070
Milos Miscevic <milos.miscevic@yahoo.com>
2020-05-18 14:37:55 +0200

    Implement staticlly served photos

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -2,6 +2,11 @@ export const StorageConfig = {
 
     photo: {
         destination: '../storage/photos/',
+        urlPrefix: '/assets/photos', // putanja u linku kojom se dolazi do fotografija
+        /*
+        setujemo za koje vreme ce slika biti kesirana u mili sekundama
+        */
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dana
         maxSize: 3 * 1024 * 1024, // 3MB u bajtovima
         resize: {
             thumb: {
